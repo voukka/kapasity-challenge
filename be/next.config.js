596b9369c9353feb5a3591c02a737669364907ca
Vladimir Grigor <vladimir.grigor@wolt.com>
2024-11-13 16:19:29 +0200

    fix CORS

--- be/next.config.js
+++ be/next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "standalone",
+  headers: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "false" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+          },
+        ],
+      },
+    ];
+  },
 };
